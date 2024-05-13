93c5c1bbfdb3a559c901099ba703e349ba25ff25
Raphael Mello <raphaelpmros@hotmail.com>
2024-05-13 16:23:06 -0300

    criando a estrutura base do crud para dar inicio ao projeto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { BooksModule } from './books/books.module';
+import { AuthorsModule } from './authors/authors.module';
+import { RentsModule } from './rents/rents.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule, BooksModule, AuthorsModule, RentsModule, ReviewsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
