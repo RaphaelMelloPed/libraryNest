67f00c9124e12e5b101dd37d2a80ad23a0bd59f1
Raphael Mello <raphaelpmros@hotmail.com>
2024-05-13 18:06:01 -0300

    deixando o create de category pronto

--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -1,11 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryEntity } from './entities/category.entity';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private categoriesRepository: Repository<CategoryEntity>,
+  ) {}
+
+  async create({ name }: CreateCategoryDto) {
+    const existingCategory = await this.categoriesRepository.findOne({
+      where: { name: name },
+    });
+
+    if (existingCategory) {
+      return 'This category already exist';
+    }
+
+    const newCategory = await this.categoriesRepository.create({ name });
+
+    return await this.categoriesRepository.save(newCategory)
+    
   }
 
   findAll() {
